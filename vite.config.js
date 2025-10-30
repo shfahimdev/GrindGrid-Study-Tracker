d342deee0b387ca3927deafc68d55bacc4a37f18
shfahimdev <shfahim.dev@gmail.com>
2025-10-30 16:41:40 +0600

    v4.0

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 7896,
     proxy: {
       '/api': {
         target: 'http://localhost:7001',
